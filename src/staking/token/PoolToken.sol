2c94ae2e115020f9f1d15a13114d433f36815428
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-13 08:13:38 +0300

    Update import path for ReentrancyGuard in Staking.sol and PoolToken.sol

--- src/staking/token/PoolToken.sol
+++ src/staking/token/PoolToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "../../utils/ERC20.sol";
 
 contract PoolToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("PoolToken", "PLT") {
