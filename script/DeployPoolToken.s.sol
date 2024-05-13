78e744bf2bd43ed6c02cf2f05de4bcc869e8603e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-13 08:37:17 +0300

    Refactor DeployLpToken contract to DeployPoolToken in DeployPoolToken.s.sol and test PooToken

--- script/DeployPoolToken.s.sol
+++ script/DeployPoolToken.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {PoolToken} from "../src/staking/token/PoolToken.sol";
 import {console} from "forge-std/console.sol";
 
-contract DeployLpToken is Script {
+contract DeployPoolToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1_000_000 ether; // 1 million tokens with 18 decimal places
     uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
     uint256 public deployerKey;
