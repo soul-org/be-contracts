5f5a7bbf9d3693769f3da9ea0058243723c37bcc
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-14 07:22:03 +0300

    Add Lottery contract to src/Lottery/Lottery.sol

---
+++ src/Lottery/Lottery.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+  * @title Lottery Contract
+  * @author Arnaud
+  * @notice You can use this contract for only the lottery
+  *@dev Implementing Chainlink VRFv2
+ */
+
+contract Lottery {
+  
+}
